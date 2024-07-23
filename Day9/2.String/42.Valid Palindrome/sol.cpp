b4da7f0c44fc36ba9e4fc9b51a9cd66fed38e3e0
DEV REDDY <devreddy4444@gmail.com>
2024-07-23 23:31:47 +0530

    Convert Binary Number in a Linked List to Integer

---
+++ Day9/2.String/42.Valid Palindrome/sol.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+
+    bool isValid(string s, int i, int j)
+    {
+        while (i < j)
+        {
+            if (s[i] != s[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+public:
+    bool validPalindrome(string s)
+    {
+        int i = 0, j = s.size() - 1;
+
+        while (i < j)
+        {
+            if (s[i] == s[j])
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                return isValid(s, i + 1, j) || isValid(s, i, j - 1);
+            }
+        }
+
+        return true;
+    }
+};

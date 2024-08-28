9fa54280454bc791cdd09a5fc81cf988bfe8eefa
DEV REDDY <devreddy4444@gmail.com>
2024-08-28 22:20:08 +0530

    Min distance between two given nodes of a Binary Tree

---
+++ Day45/Tree/210.Min distance between two given nodes of a Binary Tree/sol.cpp
@@ -0,0 +1,44 @@
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+class Solution
+{
+public:
+    /* Should return minimum distance between a and b
+    in a tree with given root*/
+    int ans = 0;
+    int traverse(Node *root, int a, int b)
+    {
+        if (root == NULL || ans > 0)
+            return 0;
+        int l = traverse(root->left, a, b);
+        int r = traverse(root->right, a, b);
+        if ((root->data == a || root->data == b))
+        {
+            if (l != 0)
+                ans = l;
+            else if (r != 0)
+                ans = r;
+            else
+                return 1;
+        }
+        if (l != 0 && r != 0)
+            ans = l + r;
+        else if (l != 0)
+            return l + 1;
+        else if (r != 0)
+            return r + 1;
+        return 0;
+    }
+    int findDist(Node *root, int a, int b)
+    {
+        if (a == b)
+            return 0;
+        traverse(root, a, b);
+        return ans;
+    }
+};
